d287d209d945262d7caf6333d3eefe6e500e5cc7
MatexConcept <matexconcept@gmail.com>
2025-02-04 20:48:38 +0100

    nextjs configured

---
+++ templates/next-app/app/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+      <footer className="bg-black/50 backdrop-blur-md py-4 text-center text-white">
+        <p>&copy; {new Date().getFullYear()} CrossFi Chain. All rights reserved.</p>
+      </footer>
+    )
+  }
+  
+  
